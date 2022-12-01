3d1fa6e805c8db3a2fa7c8c9bbd458622d748a21
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-01 06:28:31 +0200

    Changes, delete import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.security.spec.RSAOtherPrimeInfo;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
