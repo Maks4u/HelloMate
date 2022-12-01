c5d746418ce909c7881798d1ef3fc21433e9529d
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-01 06:13:55 +0200

    Add a new frase to uor programm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
-package org.example;public class Main {
+package org.example;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello mates!");
     }
 }
